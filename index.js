ccb55d26d9d45074cbcee39e4a7fde28807ee650
Jason Hargrove <rijaha@gmail.com>
2024-04-29 21:17:44 -0500

    creating a prompt

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ import OpenAI from 'openai'
 const openai = new OpenAI()
 
 async function hello() {
-  const completion = await openai.chat.completions.create({
+  const stream = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: 'You are a greate robot!' },
+      {
+        role: 'system',
+        content:
+          'You are a great motivational speaker and artist who encourages me as an artist to keep studying and doing the hard work necessary for success',
+      },
       {
         role: 'user',
-        content: 'What is the closest ski resort to Paduca, Kentucky?',
+        content:
+          'What do I need to study to be a great artist who takes advantage of the latest techniques',
       },
     ],
     model: 'gpt-3.5-turbo',
+    stream: true,
   })
-  console.log(completion.choices[0])
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0].delta.content || '')
+  }
 }
 
 hello()
